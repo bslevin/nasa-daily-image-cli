1b52911a2eb634660ca7f9437663ef7260d5f91f
bslevin <blogs82@yahoo.com>
2022-07-06 10:11:58 +0000

    test welcome function

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import typer
+import time
+
+
+WELCOME_ASCII = """\nAre you ready to explore...
+
+███████╗██████╗  █████╗  ██████╗███████╗
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝
+███████╗██████╔╝███████║██║     █████╗
+╚════██║██╔═══╝ ██╔══██║██║     ██╔══╝
+███████║██║     ██║  ██║╚██████╗███████╗
+╚══════╝╚═╝     ╚═╝  ╚═╝ ╚═════╝╚══════╝
+"""
 
 app = typer.Typer()
 
 
 @app.command()
-def hello(name):
+def hello():
     '''test function'''
+    typer.echo(WELCOME_ASCII)
+    time.sleep(2)
+    name = input("Please enter your name: ")
     typer.echo(f"Hello {name}")
 
 
